533a937369efa975cee74ef4bdeb4d5bc74973ae
A.Grodel <98063447+Grodelek@users.noreply.github.com>
2025-02-17 23:08:49 +0100

    Add files via upload

---
+++ lab10/app/kontakt/page.js
@@ -0,0 +1,12 @@
+import ContactForm from './components/ContactForm';
+
+export default function KontaktPage() {
+  return (
+    <div>
+        <p>Witaj</p>
+        <p>Masz pytania? Z chęcia na nie odpowiem!</p>
+        <p>Skontaktuj się ze mną, wypełniając poniższy formularz:</p>
+      <ContactForm />
+    </div>
+  );
+}
---
+++ lab10/app/page.js
@@ -0,0 +1,24 @@
+import Header from './components/Header';
+
+export default function HomePage() {
+    return (
+        <div>
+            <Header title="O mnie" />
+            <p>
+                Cześć! Jestem studentem informatyki z pasją do programowania, nowych technologii i nauki. Moje główne zainteresowania koncentrują się na backendzie, a szczególnie na pracy z Javą oraz popularnymi technologiami takimi jak Spring i Hibernate. Zajmuję się głównie tworzeniem aplikacji serwerowych, gdzie dbam o wydajność, bezpieczeństwo i skalowalność systemów.
+            </p>
+            <p>
+                W swojej pracy wykorzystuję Java Spring do budowy solidnych i skalowalnych aplikacji webowych, a także Hibernate do efektywnego zarządzania bazami danych. Znam również inne technologie, takie jak RESTful API, SQL oraz systemy baz danych (MySQL, PostgreSQL). Chociaż jestem skoncentrowany na backendzie, staram się również zrozumieć i współpracować z frontendem, by lepiej rozumieć całość procesu tworzenia aplikacji.
+            </p>
+            <p>
+                Moje umiejętności obejmują projektowanie i implementację usług backendowych, zarządzanie bazami danych oraz integrację różnych systemów. Interesuje mnie także optymalizacja wydajności aplikacji i rozwiązywanie problemów związanych z dużymi zbiorami danych. Regularnie śledzę nowe technologie i narzędzia, co pozwala mi być na bieżąco z najnowszymi trendami w świecie backendu.
+            </p>
+            <p>
+                W wolnym czasie lubię uczestniczyć w hackathonach, gdzie mogę sprawdzić swoje umiejętności w rywalizacji, a także rozwijać swoją kreatywność. Dodatkowo, interesuję się także fotografią, grami komputerowymi oraz podróżami. Wierzę, że dobra równowaga między pracą a pasjami pozwala mi być bardziej efektywnym w codziennym życiu.
+            </p>
+            <p>
+                Zapraszam do zapoznania się z moim portfolio, gdzie znajdziesz więcej informacji o moich projektach, doświadczeniach zawodowych oraz o mnie. Mam nadzieję, że moje prace zainspirują Cię do współpracy lub pomogą w realizacji Twoich projektów!
+            </p>
+        </div>
+    );
+}
